a6c87c2260c4cd9747e4a40f5a333b29d4aa9003
Gfrim <gfrim79@gmail.com>
2022-07-01 07:21:08 -0700

    Patience, persistence and perspiration make an unbeatable combination for success

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	signed int a = 48;
+
+	while (a <= 57)
+	{
+		if (a == 57)
+			putchar(a);
+		else
+		{
+			putchar(a);
+			putchar(44);
+			putchar(32);
+		}
+		a++;
+	}
+	putchar(10);
+	return (0);
+}
