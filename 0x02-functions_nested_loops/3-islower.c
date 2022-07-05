c485f3aa0d5320d107af2cb9b9edb7d6d068630f
Gfrim <tophcker4@gmail.com>
2022-07-05 13:28:32 +0000

    islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,8 +3,8 @@
 
 /**
  * _islower - checks for lowercase character.
+ * c - Decimal of a character based on ASCII.
  *
- * int c - Decimal of a character based on ASCII
  * Return: 1 (True) or 0 (False)
  */
 int _islower(int c)
