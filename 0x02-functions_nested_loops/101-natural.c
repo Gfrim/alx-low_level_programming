ed2ee61f3f215c112ce71b810e6b95547a8f0e0c
Gfrim <tophcker4@gmail.com>
2022-07-05 19:23:13 +0000

    Nature made the natural numbers; All else is the work of women

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 /**
- *  * main - check for multiples of 3 and 5
- *   *
- *    * Return: 0 always
- *     */
+ * main - check for multiples of 3 and 5
+ *
+ * Return: 0 always
+ */
 int main(void)
 {
 	int x = 1024, y, sum = 0;
+
 	for (y = 0; y < x; y++)
 	{
 		if ((y % 3 == 0) || (y % 5 == 0))
