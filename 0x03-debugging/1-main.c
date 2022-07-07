8e388c884085007d830f8255d88610c1bfb16509
Gfrim <gfrim79@gmail.com>
2022-07-07 06:01:32 -0700

    Like, comment, subscribe

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -7,18 +7,18 @@
 
 int main(void)
 {
-	/*int i;*/
-	/*i = 0;*/
+	int i;
+
+	i = 0;
+
+	printf("Infinite loop incoming :(\n");
+
 	/**
 	 * while (i < 10)
 	 * {
 	 * 	putchar(i);
 	 * }
 	 */
-	
-	printf("Infinite loop incoming :(\n");
-
 	printf("Infinite loop avoided! \\o/\n");
-	
 	return (0);
 }
