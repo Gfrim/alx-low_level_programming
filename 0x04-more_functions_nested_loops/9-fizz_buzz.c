280a891e28e16f9a2a4acb2f32dd70ffbcbb81dc
Gfrim <gfrim79@gmail.com>
2022-07-07 18:32:42 -0700

    Fizz-Buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - Entery Point
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	i = 1;
+	while (i <= 100)
+	{
+		if ((i % 3 == 0) && (i % 5 == 0))
+			printf("FizzBuzz ");
+		else if (i % 3 == 0)
+			printf("Fizz ");
+		else if (i % 5 == 0)
+			printf("Buzz ");
+		/*else if ((i % 3 == 0) && (i % 5 == 0))*/
+		/*	printf("FizzBuzz ");*/
+		else
+		{
+			printf("%d ",i);
+		}
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
