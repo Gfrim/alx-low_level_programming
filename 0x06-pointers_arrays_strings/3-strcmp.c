d5ddd147fdf9b73772ba4d8e64f51ff427218456
Gfrim <gfrim79@gmail.com>
2022-07-13 09:49:35 -0700

    strcmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strcmp - Compares two strings
+ * @s1: First string
+ * @s2: Second string
+ *
+ * Return: int
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	while (*s1)
+	{
+		if (*s1 != *s2)
+			break;
+		s1++;
+		s2++;
+	}
+
+	return *(const unsigned char*)s1 - *(const unsigned char*)s2;
+}
