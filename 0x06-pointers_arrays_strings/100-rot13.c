1019dd1cda132851cb6b2a13eefe14ae19384c10
Gfrim <gfrim79@gmail.com>
2022-07-13 18:40:31 -0700

    rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * leet - encodes a string using rot13
+ * @s: string to be passed
+ *
+ * Return: char
+ */
+
+char *leet(char *s)
+{
+	int a, b;
+
+	char *i = "ABCDEFGHIJKLMNOPQRSTUVWXYSabcdefghijklmnopqrstuvwxyz";
+	char *j = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (a = 0; s[a] != '\0'; a++)
+	{
+		for (b = 0; i[b] != '\0'; b++)
+		{
+			if (s[a] == i[b])
+			{
+				s[a] = j[b];
+				break;
+			}
+		}
+	}
+
+	return (s);
+}
