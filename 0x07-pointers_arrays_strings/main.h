d5bebfaa2e301d527d3f33a28ec699696f82a754
Gfrim <gfrim79@gmail.com>
2022-07-18 12:22:57 -0700

    Update header file

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -3,3 +3,4 @@ char *_memset(char *, char, unsigned int );
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
 unsigned int _strspn(char *, char *);
+char *_strpbrk(char *s, char *accept);
