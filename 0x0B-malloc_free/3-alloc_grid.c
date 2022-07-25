52a558779a2bcb34fb11390b3f10a536582be532
Gfrim <gfrim79@gmail.com>
2022-07-25 09:42:57 -0700

    Creating a two dimensional array grid

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -17,7 +17,7 @@ int **alloc_grid(int width, int height)
 	if (width <= 0 || height <= 0)
 		return (NULL);
 
-	array = malloc(sizeof(**array) * height);
+	array = malloc(sizeof(int *) * height);
 
 	if (array == NULL)
 		return (NULL);
