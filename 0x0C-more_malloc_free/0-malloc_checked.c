2ccfda28820bf17c0d86ad7d0692b3c36bd95e20
Gfrim <gfrim79@gmail.com>
2022-07-28 07:39:12 -0700

    allocating memory dynamically with malloc

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * malloc_checked - Allocating memory using malloc
+ * @b: Parameter
+ *
+ * Return: Pointer
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	unsigned int *i;
+
+	while (1)
+	{
+		i = malloc(INT_MAX);
+	}
+	_putchar(98);
+}
