5e3d668015cd438d0d7cb3cd691e8ec27e8cdf14
Gfrim <gfrim79@gmail.com>
2022-07-29 06:42:20 -0700

    Updated header file

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -11,4 +11,10 @@ char *string_nconcat(char *, char *, unsigned int );
 void *_calloc(unsigned int , unsigned int );
 int *array_range(int , int );
 void *_realloc(void *, unsigned int, unsigned int);
+int find_len(char *str);
+char *_memset(char *s, char b, usigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
+int onlyNumbers(char *c);
+void multiply(char* s1, char* s2);
 
+#endif
