81c61414fcc5090be1bd3dfe38271f1ec1db52e6
Gfrim <gfrim79@gmail.com>
2022-07-29 02:29:59 -0700

    Updated header file

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -2,3 +2,4 @@ int _putchar(char );
 void *malloc_checked(unsigned int );
 char *string_nconcat(char *, char *, unsigned int );
 void *_calloc(unsigned int , unsigned int );
+int *array_range(int , int );
