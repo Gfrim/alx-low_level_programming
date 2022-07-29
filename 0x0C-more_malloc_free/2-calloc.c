c51b42fcfdb6ec9fb8e729105b69aaa1abd0511a
Gfrim <gfrim79@gmail.com>
2022-07-29 01:32:43 -0700

    Allocates memory using malloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -22,8 +22,8 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 		for (i = 0; i < (nmemb * size); i++)
 		{
 			array[i] = 0;
+			return (array);
 		}
-		return (array);
 	}
 	else
 		return (NULL);
