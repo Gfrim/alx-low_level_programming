61990e10a8fb8ebbd6ab20ada5dc2451125d16f0
Gfrim <gfrim79@gmail.com>
2022-07-29 04:49:02 -0700

    Reallocating memory

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,43 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _realloc - Reallocates a memory block
+ * @ptr: pointer to current memory block
+ * @old_size: size for ptr
+ * @new_size: size for new memory block
+ *
+ * Return: Pointer
+ */
+
+void *_realloc(void *ptr, unsigned int old_size, unsigned new_size)
+{
+	void *_newblock;
+	unsigned int i;
+
+	if (ptr == NULL)
+	{
+		_newblock = malloc(new_size);
+		return (_newblock);
+	}
+	else if (new_size == old_size)
+		return (ptr);
+	else if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	else
+	{
+		_newblock = malloc(new_size);
+		if (_newblock != NULL)
+		{
+			for (i = 0; i < min(old_size, new_size); i++)
+				*((char *)_newblock + i) = *((char *) ptr + i);
+			free(ptr);
+			return (_newblock);
+		}
+		else
+			return (NULL);
+	}
+}
