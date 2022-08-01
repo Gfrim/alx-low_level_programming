a69df11fd320864e12d1c61950f4375432d4a478
Gfrim <gfrim79@gmail.com>
2022-08-01 12:57:51 -0700

    Performing addition with defined macro

---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,15 @@
+#ifndef SUM
+#define SUM
+
+/**
+ * SUM - performing addition
+ * @x: first parameter
+ * @y: second parameter
+ * Return: sum
+ */
+
+int SUM(int x, int y)
+{
+	return (x + y);
+}
+#endif
