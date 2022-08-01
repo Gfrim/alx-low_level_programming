713f5a698bca68e587edd06b3535fc919ac76222
Gfrim <gfrim79@gmail.com>
2022-08-01 13:48:03 -0700

    Create new dog from struct

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,46 @@
+#include "dog.h"
+
+/**
+ *new_dog - creates a new dog
+ *@name: name of new dog
+ *@age: age of new dog
+ *@owner: owner of new dog
+ *Return: returns NULL in case
+ *of failure
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	unsigned int i, namel, ownerl;
+	dog_t *dog;
+
+	if (name == NULL || owner == NULL)
+		return (NULL);
+	dog = malloc(sizeof(dog_t));
+	if (dog == NULL)
+		return (NULL);
+	for (namel = 0; name[namel]; namel++)
+		;
+	namel++;
+	dog->name = malloc(namel * (sizeof(char)));
+	if (dog->name == NULL)
+	{
+		free(dog);
+		return (NULL);
+	}
+	for (i = 0; i < namel; i++)
+	dog->name[i] = name[i];
+	dog->age = age;
+	for (ownerl = 0; owner[ownerl]; ownerl++)
+		;
+	ownerl++;
+	dog->owner = malloc(ownerl * (sizeof(char)));
+	if (dog->owner == NULL)
+	{
+		free(dog->name);
+		free(dog);
+		return (NULL);
+	}
+	for (i = 0; i < ownerl; i++)
+	dog->owner[i] = owner[i];
+	return (dog);
+}
