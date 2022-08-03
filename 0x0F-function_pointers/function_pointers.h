50154726c91fa25bfb265aafc580e28027f5675a
Gfrim <gfrim79@gmail.com>
2022-08-03 07:58:42 -0700

    Updated header file

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,7 +1,10 @@
 #ifndef pointer_to_function_H
 #define pointer_to_function_H
 
-int _putchar(char );
+#include <stdio.h>
+#include <stdlib.h>
+
+int _putchar(char);
 void print_name(char *name, void (*f)(char *));
 
 #endif
