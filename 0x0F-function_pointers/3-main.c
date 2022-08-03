af3a2363b48b78beaca9af9f056966ed3d852904
Gfrim <gfrim79@gmail.com>
2022-08-03 11:50:42 -0700

    Main file to test on functions

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -3,35 +3,33 @@
 #include <stdlib.h>
 
 /**
- * main - Entry level
- * @argc: number of arguments
- * @argv: array of pointers to arguments
- *
- * Return: 0 Success
+ * main - Prints the result of simple operations.
+ * @argc: The number of arguments supplied to the program.
+ * @argv: An array of pointers to the arguments.
+ * Return: Always 0.
  */
-
-int main(int argc, char *argv[])
+int main(int __attribute__((__unused__)) argc, char *argv[])
 {
-	int f_num, s_num;
+	int num1, num2;
 	char *op;
 
 	if (argc != 4)
 	{
-		printf("Error\n");
-		exit(98);
+	printf("Error\n");
+	exit(98);
 	}
-	f_num = atoi(argv[1]);
+	num1 = atoi(argv[1]);
 	op = argv[2];
-	s_num = atoi(argv[3]);
+	num2 = atoi(argv[3]);
 	if (get_op_func(op) == NULL || op[1] != '\0')
 	{
-		printf("Error\n");
-		exit(99);
+	printf("Error\n");
+	exit(99);
 	}
 	if ((*op == '/' && num2 == 0) || (*op == '%' && num2 == 0))
 	{
-		printf("Error\n");
-		exit(100);
+	printf("Error\n");
+	exit(100);
 	}
 	printf("%d\n", get_op_func(op)(num1, num2));
 	return (0);
