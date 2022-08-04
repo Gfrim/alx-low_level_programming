dc8a80c682258b6cee15b8ac3657e7ae202a80fa
Gfrim <gfrim79@gmail.com>
2022-08-04 16:20:01 -0700

    print all types

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -20,38 +20,34 @@ void print_all(const char * const format, ...)
 		switch (format[i])
 		{
 			case 'c':
-				switch (j)
-				{case 1:printf(", ");}
-				j = 1;
-				printf("%c", va_arg(ap, int));
-				break;
-			case 'i':
-				switch (j)
-				{case 1:printf(", ");}
-				j = 1;
-				printf("%i", va_arg(ap, int));
-				break;
-			case 'f':
-				switch (j)
-				{case 1:printf(", ");
-				}
-				j = 1;
-				printf("%f", va_arg(ap, double));
-				break;
-			case 's':
-				switch (j)
-				{case 1:printf(", ");
-				}
-				j = 1;
-				c = va_arg(ap, char *);
-				if (c)
-				{printf("%s", c);
-				break;}
-				printf("%p", c);
-				break;
-		}
-		i++;
-	}
+			switch (j)
+			{ case 1:printf(", "); }
+			j = 1;
+			printf("%c", va_arg(ap, int));
+			break;
+		case 'i':
+			switch (j)
+			{ case 1:printf(", "); }
+			j = 1;
+			printf("%i", va_arg(ap, int));
+			break;
+		case 'f':
+			switch (j)
+			{ case 1:printf(", "); }
+			j = 1;
+			printf("%f", va_arg(ap, double));
+			break;
+		case 's':
+			switch (j)
+			{ case 1:printf(", "); }
+			j = 1;
+			c = va_arg(ap, char *);
+			if (c)
+			{ printf("%s", c);
+			break; }
+			printf("%p", c);
+			break; }
+	i++; }
 	printf("\n");
 	va_end(ap);
 }
