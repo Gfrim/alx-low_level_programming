8340823d053045dd86a75074dfd0382cedcecaf3
Gfrim <gfrim79@gmail.com>
2022-08-11 08:03:53 -0700

    Function to print all elements of a single linked list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+
+/**
+ * print_list - Prints all elements of a list_t list
+ * @h: pointer to the head
+ *
+ * Return: Elements in the list
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t c = 0;
+	const list_t *ptr = NULL;
+
+	if (h == NULL)
+		printf("List is empty\n");
+
+	ptr = h;
+
+	while (ptr != NULL)
+	{
+		c++;
+		if (ptr->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n", ptr->len, ptr->str);
+		ptr = ptr->next;
+	}
+	return (c);
+}
