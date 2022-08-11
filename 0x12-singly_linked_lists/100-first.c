e9ea7aa25acf23a7372ea53d14179606e4e84eec
Gfrim <gfrim79@gmail.com>
2022-08-11 16:42:50 -0700

    Constructor

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
-void __attribute__((constructor)) first(void);
+void __attribute__((constructor)) premain(void);
 
 /**
  * first - function that gets executed before main
  * Return: None
  */
 
-void first(void)
+void premain(void)
 {
 	printf("You're beat! and yet, you must allow, \n"
 	"I bore my house upon my back!\n");
