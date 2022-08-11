ab5c423d71bc124fee338c1092c415f6eca42112
Gfrim <gfrim79@gmail.com>
2022-08-11 16:07:04 -0700

    Constructor

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * first - function that gets executed before main
+ * Return: None
+ */
+
+void __attribute__ ((constructor)) first()
+{
+	printf("You're the beat! and yet, you must allow,");
+	printf("\nI bore my house upon my back!\n");
+}
