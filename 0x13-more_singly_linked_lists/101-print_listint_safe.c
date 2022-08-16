a801137dde1659d9b020e20974d6195d41c8a936
Gfrim <gfrim79@gmail.com>
2022-08-16 07:42:42 -0700

    Prints a linked list in a safer way

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,68 +1,35 @@
 #include "lists.h"
 #include <stdio.h>
-
-/**
- * free_listpoint - frees a linked list
- * @head: pointer to a node
- *
- * Return: void
- */
-
-void free_listpoint(listpoint **head)
-{
-	listpoint *temp, *curr;
-
-	if (head != NULL)
-	{
-		curr = *head;
-		while ((temp = curr) != NULL)
-		{
-			curr = curr->next;
-			free(temp);
-		}
-		*head = NULL;
-	}
-}
-
 /**
  * print_listint_safe - Print a `listint_t` linked list including mem addresses
  * @head: head of linked list
  * Description: Go through the list only once.
  * Return: number of nodes in list. If fails, exit with status 98.
  */
-size_t print_listint_safe(const listpoint *head)
+size_t print_listint_safe(const listint_t *head)
 {
-	listpoint *current, *new, *add;
-	size_t count = 0;
+	const listint_t *current;
+	size_t count;
+	const listint_t *hold;
 
-	current = NULL;
-	while (head != NULL)
+	current = head;
+	if (current == NULL)
+		exit(98);
+
+	count = 0;
+	while (current != NULL)
 	{
-		new = malloc(sizeof(listpoint));
-
-		if (new == NULL)
-			exit(98);
-
-		new->p = (void *)head;
-		new->next = current;
-
-		add = current;
-
-		while (add->next != NULL)
-		{
-			add = add->next;
-			if (head == add->p)
-			{
-				printf("[%p] %d\n", (void *)head, head->p);
-				free_listpoint(&current);
-				return (count);
-			}
-		}
-
-		printf("[%p] %d\n", (void *)head, head->n);
-		head = head->next;
+		hold = current;
+		current = current->next;
 		count++;
+		printf("[%p] %d\n", (void *)hold, hold->n);
+
+		if (hold < current)
+		{
+			printf("-> [%p] %d\n", (void *)current, current->n);
+			break;
+		}
 	}
-	free_listpoint(&current);
+
 	return (count);
 }
