a4a261a79e8db1e9fb4a96d64422724375b14546
Gfrim <gfrim79@gmail.com>
2022-08-22 08:24:43 -0700

    Updated header file

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -10,5 +10,7 @@ int _putchar(char);
 ssize_t read_textfile(const char *, size_t);
 int create_file(const char *, char *);
 int append_text_to_file(const char *, char *);
+char *create_buf(char *);
+void close_file(int);
 
 #endif
